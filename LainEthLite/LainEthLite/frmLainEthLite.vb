25bc3367371913a4e5a398386d070474b084eb4a
MrJag <Anthony@.(none)>
2008-06-20 15:25:13 -0500

    Version string update.

--- LainEthLite/LainEthLite/frmLainEthLite.vb
+++ LainEthLite/LainEthLite/frmLainEthLite.vb
@@ -22,7 +22,7 @@ Public Class frmLainEthLite
     Private Const GAME_PRIVATE As Integer = 17
     Private Const GAME_PUBLIC As Integer = 16
 
-    Public Const ProjectLainVersion As String = "Battle.net Game Host v0.15 Beta"
+    Public Const ProjectLainVersion As String = "Battle.net Game Host v0.16 (beta)"
     Public Const ProjectLainName As String = "LainEthLite"
     Public Const ProjectLainConfig As String = "LainEthLiteConfiguration"
     Public Const ProjectLainMap As String = "LainEthMap"
